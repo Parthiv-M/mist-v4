1ee0ea0b436114d66c70e28c968e051d314a4207
Parthiv-M <parthiv.menon@learner.manipal.edu>
2022-08-18 14:42:24 +0530

    updated aos import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { AnimatePresence } from 'framer-motion';
-import AOS from 'aos';
+const AOS = require('aos');
 
 // import aos styles
 import 'aos/dist/aos.css';
